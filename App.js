1c0ba42d514d3907d331098a8b51be13f7008b6d
João Araujo <joao.araujo@yaman.com.br>
2020-03-24 19:18:16 -0300

    second commit

--- App.js
+++ App.js
@@ -2,11 +2,11 @@ import React, {useState} from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
-  const [outputText, setoutText] = useState('Aperta essa porra, krl! Quer ficar rico nao seu fdp?!');
+  const [outputText, setoutText] = useState('Aperte o Botão para gerar os números');
   return (
     <View style={styles.container}>
       <Text>{outputText}</Text>
-      <Button title ="Aperta ai Fdp" onPress = {() => 
+      <Button title ="Botão" onPress = {() => 
         {setoutText(megaSena())}}/>
     </View>
   );
